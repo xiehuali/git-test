ab1f33b0ebd1fec77ba59a6ee3f233af1313fc29
xiehuali <1194954375@qq.com>
2023-02-01 16:30:40 +0800

    first-commit

---
+++ src/main/java/com/syeet/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.syeet.git;
+
+/**
+ * @author xhl
+ * @date 2023/2/1
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
