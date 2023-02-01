4e00f7705719798861ccb9aad3994fbeffda2bf0
xiehuali <1194954375@qq.com>
2023-02-01 16:35:21 +0800

    second-commit

--- src/main/java/com/syeet/git/GitTest.java
+++ src/main/java/com/syeet/git/GitTest.java
@@ -7,5 +7,6 @@ package com.syeet.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
